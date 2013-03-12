4cc74e4d7620565061ba70991873da6d2268faed
brianpetro <brian@plexm.com>
2013-03-11 23:14:24 -0400

    add root route and config

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Brianpetro::Application.routes.draw do
   resources :entries
-
+	root to: 'entries#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
