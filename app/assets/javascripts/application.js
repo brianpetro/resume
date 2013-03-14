d3e48a800db996f317f94b52aa5688ce7aaf9772
brianpetro <brian@plexm.com>
2013-03-14 13:32:19 -0400

    stop requiring turbolinks and bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require bootstrap
 //= require angular
 //= require angular-resource
 //= require angular-ui-bootstrap
-//= require turbolinks
 //= require_tree .
