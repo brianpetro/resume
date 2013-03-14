6c7cfdaed019dc2565ceaa4897b06933e0bfdb29
brianpetro <brian@plexm.com>
2013-03-14 12:36:41 -0400

    add resume_pdf.rb

---
+++ app/pdfs/resume_pdf.rb
@@ -0,0 +1,53 @@
+class ResumePdf < Prawn::Document
+
+	def initialize(entries)
+		super(top_margin: 70)
+		font "Times-Roman"
+		resume_header
+		cover_statement
+		contact_info
+		list_experiences(entries)
+	end
+	
+	def resume_header
+		text "Brian Petro", size: 32
+		move_down 20
+	end
+	
+	def cover_statement
+		text "If what you are looking for is a self-motivated and technically apt team member, contact me and continue this conversation."
+		move_down 20
+	end
+	
+	def contact_info
+		text "Brian@plexm.com"
+		text "267-474-8036"
+		text "70 Berkshire Lane"
+		text "Palm Coast, FL 32137"	
+	end
+	
+	def list_experiences(entries)
+
+		# Begin Projects	
+		start_new_page
+		text "Projects", size: 24
+		move_down 10
+		entries.where(isProject: true).each do |e|
+			text e.title, size: 16
+			text "- #{e.details}"
+			text " "
+		end
+		
+		#Begin Techs
+		start_new_page
+		text "Dev Tools", size: 24
+		move_down 10
+		entries.where(isTech: true).each do |e|
+			text e.title, size: 16
+			text "- #{e.details}"
+			text " "
+		end
+		
+	end
+	
+end
