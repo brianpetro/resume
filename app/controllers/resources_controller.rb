0bbad16d773c876f3982ff761b39103bb7a1ff63
brianpetro <brian@plexm.com>
2013-04-05 17:45:53 -0400

    skip before filter preventing persistence.

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,4 +1,5 @@
 class ResourcesController < ApplicationController
+	skip_before_filter :verify_authenticity_token, :only => [:create]
   before_action :set_resource, only: [:show, :edit, :update, :destroy]
 
   # GET /resources
