321eae95062ddf1a0bd96c53f0a02664912c1462
brianpetro <brian@plexm.com>
2013-04-29 15:09:28 -0400

    Add upvote method to Qscore

--- app/models/qscore.rb
+++ app/models/qscore.rb
@@ -1,4 +1,12 @@
 class Qscore < ActiveRecord::Base
   belongs_to :tag
   belongs_to :resource
+
+  def self.upvote(qscore_id)
+  	qscore = Qscore.find(qscore_id)
+  	qv = qscore.value.to_i
+  	newQV = qv + 1
+  	qscore.value = newQV
+  	qscore.save
+  end
 end
