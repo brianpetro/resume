787562198b0060316c86e9ac48d01d627ff1bdc7
brianpetro <brian@plexm.com>
2013-04-29 15:19:31 -0400

    change method name

--- app/models/qscore.rb
+++ app/models/qscore.rb
@@ -2,7 +2,7 @@ class Qscore < ActiveRecord::Base
   belongs_to :tag
   belongs_to :resource
 
-  def self.upvote(qscore_id)
+  def self.upvote_method(qscore_id)
   	qscore = Qscore.find(qscore_id)
   	qv = qscore.value.to_i
   	newQV = qv + 1
