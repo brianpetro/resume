e41f4c8d4bcb6e57a1563f1ac420bf00112e4887
brianpetro <brian@plexm.com>
2013-06-18 11:40:49 -0400

    prepare for production test

--- config/application.rb
+++ config/application.rb
@@ -23,11 +23,11 @@ module Brianpetro
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.action_mailer.delivery_method = :smtp
+		config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
 			:address              => "smtp.gmail.com",
 			:port                 => 587,
-			:domain               => "yourdomain.dev",
+			:domain               => "plexm.com",
 			:user_name            => ENV['GMAIL_EMAIL'],
 			:password             => ENV['GMAIL_PASS'],
 			:authentication       => :plain,
@@ -35,7 +35,7 @@ module Brianpetro
 		}
 
 		config.action_mailer.default_url_options = {
-			:host => "yourdomain.dev"
+			:host => "plexm.com"
 		}
   end
 end
